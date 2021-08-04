3e0223b7029447676f65211fcf5cfc0a7346fb9c
goulartf <fgoulart.nas@gmail.com>
2021-08-03 21:02:23 -0400

    gh-pages

---
+++ precache-manifest.a766ff7ab58b0796a30ffd8aa0400b33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d983f480f199424246c",
+    "url": "/olympics-site/css/app.20018191.css"
+  },
+  {
+    "revision": "b818bb91dcee8953b359",
+    "url": "/olympics-site/css/chunk-vendors.45d036d2.css"
+  },
+  {
+    "revision": "8e2fac55bed04305cd4e4a93894ee711",
+    "url": "/olympics-site/index.html"
+  },
+  {
+    "revision": "5d983f480f199424246c",
+    "url": "/olympics-site/js/app.9a4d4256.js"
+  },
+  {
+    "revision": "b818bb91dcee8953b359",
+    "url": "/olympics-site/js/chunk-vendors.62fa3c11.js"
+  },
+  {
+    "revision": "b7ecc9594fcf92485a1ffb9ce214b3d3",
+    "url": "/olympics-site/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/olympics-site/robots.txt"
+  }
+]);
